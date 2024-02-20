9b770ef87bd463cedf9e8b7ad0f3f7cbe7a754f8
Théo ALVES DA COSTA <theo.alvesdacosta@ekimetrics.com>
2024-02-20 07:19:04 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://climateqa.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/climateqa_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
